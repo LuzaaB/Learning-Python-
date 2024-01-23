ee76ad3dd5e38c0ca1ac5edc1618533010bea51f
Luzaa Byanjankar <luzaabyanjankar123@gmail.com>
2024-01-23 14:41:39 +0545

    End of Basics

--- Basics/basics.py
+++ Basics/basics.py
@@ -1,4 +1,10 @@
 age = 20
 name ='John'
+
+# format function
 print('{0} was {1} years old when he wrote this book'.format(name,age))
 print('Why is {0} playing with python?'.format(name))
+
+# end funciton
+print('a', end='')
+print('b')
