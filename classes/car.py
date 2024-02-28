2b53b33d4067d6e2b867b7820e5ab154a28fc9f2
Luzaa Byanjankar <luzaabyanjankar123@gmail.com>
2024-02-28 18:13:29 +0545

    add instance method example in car.py file

--- classes/car.py
+++ classes/car.py
@@ -7,3 +7,29 @@ class Car:
         self.started = False
         self.speed = 0
         self.max_speed = 200
+        
+    # Example of Instance Method
+    def start(self):
+        print("Starting the car ...")
+        self.started = True
+    
+    def stop(self):
+        print("Stopping the car ...")
+        self.started = False
+        
+    def accelerate(self, value):
+        if not self.started:
+            print("Car has not started!")
+            return
+        if self.speed + value <= self.max_speed:
+            self.speed += value
+        else:
+            self.speed = self.max_speed
+        print(f"Accelerating to {self.speed} km/h...")
+        
+    def brake(self, value):
+        if self.speed - value >= 0:
+            self.speed -= value
+        else:
+            self.speed = 0
+        print(f"Braking to {self.speed} km/h...")
