7c0bd407c7aacb1bdd4cbfeaf4a497cca4ba7f0f
Luzaa Byanjankar <luzaabyanjankar123@gmail.com>
2024-02-28 14:34:20 +0545

    Add square.py and shapes.py

--- classes/circle.py
+++ classes/circle.py
@@ -1,8 +1,19 @@
 import math 
 
 class Circle:
+    
     def __init__(self, radius):
         self.radius = radius
+    
+    @property
+    def radius(self):
+        return self.radius
+    
+    @radius.setter
+    def radius(self,value):
+        if not isinstance(value, int|float) or value <= 0 :
+            raise ValueError("Positive number expected")    
+        self.radius = value
+        
     def calculate_area(self):
         return round(math.pi * self.radius ** 2, 2)
-        
--- classes/circle_values.py
+++ classes/circle_values.py
@@ -11,3 +11,12 @@ print("Area of first circle : ", circle_1.calculate_area())
 
 print("Radius of second circle : ", circle_2.radius)
 print("Area of second circle", circle_2.calculate_area())
+
+cicrle_3 = Circle(100)
+print(f"Radius of third circle : {cicrle_3}")
+
+circle_4 = Circle(0)
+print(circle_4)
+
+circle_5 = Circle("200")
+print(circle_5)
