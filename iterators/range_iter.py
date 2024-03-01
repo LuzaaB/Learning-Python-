75337f221f5d79514b25cf2bfce06d41b458e8d0
Luzaa Byanjankar <luzaabyanjankar123@gmail.com>
2024-03-01 13:47:43 +0545

    complete the program

--- iterators/range_iter.py
+++ iterators/range_iter.py
@@ -12,3 +12,26 @@
 #
 ####   must be repeatable after finish
 ###### must NOT have a pre-stored sequence: only lower and upper limits
+
+class RangeIterator:
+    def __init__(self, start=0, stop=None, step=None) -> None:
+        if stop is None:
+            stop, start = start, 0
+        if step is None:
+            step = 1
+        self._range = range(start, stop, step)
+        self._iter = iter(self._range)
+        
+    def __iter__(self):
+        return self
+    
+    def __next__(self):
+        try:
+            return next(self._iter)
+        except StopIteration:
+            self._iter = iter(self._range)
+            raise
+        
+numbers = RangeIterator(2,10,2)
+for num in numbers:
+    print(num)
