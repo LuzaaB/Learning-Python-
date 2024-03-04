f7ff2d916a341e211325711247c105f2e176d7e2
Luzaa Byanjankar <luzaabyanjankar123@gmail.com>
2024-03-04 11:34:20 +0545

    add math_pipeline.py file

---
+++ iterators/math_pipeline.py
@@ -0,0 +1,19 @@
+def to_square(numbers):
+    return (number ** 2 for number in numbers)
+
+def to_cube(numbers):
+    return (number ** 3 for number in numbers)
+
+def to_even(numbers):
+    return (number for number in numbers if number % 2 == 0)
+
+def to_odd(numbers):
+    return (number for number in numbers if number % 2 != 0)
+
+def to_string(numbers):
+    return (str(number) for number in numbers)
+
+
+sample  = range(20)
+list (to_cube(to_even(sample)))
+list(to_square(to_odd(sample)))
